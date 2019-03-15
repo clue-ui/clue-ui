a5fb3fa6126c70c831993eb681c522ff0cdfff93
huangchao <ftchao@local.com.cn>
2019-03-15 13:55:42 +0800

    fix [wysiwyg-quill] xx

--- components/wysiwyg-quill/src/index.vue
+++ components/wysiwyg-quill/src/index.vue
@@ -5,18 +5,20 @@
       <!-- Add font size dropdown -->
       <select class="ql-size">
         <option value="small"></option>
-        <!-- Note a missing, thus falsy value, is used to reset to default -->
         <option selected></option>
         <option value="large"></option>
         <option value="huge"></option>
       </select>
+
       <!-- Add a bold button -->
       <button class="ql-bold"></button>
+
       <!-- Add subscript and superscript buttons -->
       <button class="ql-script" value="sub"></button>
       <button class="ql-script" value="super"></button>
     </div>
 
+    <!-- editor container -->
     <div ref="editor-container">
       <p>Hello World!</p>
       <p>Some initial <strong>bold</strong> text</p>
@@ -40,6 +42,23 @@ import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 export default {
   name: 'c-wysiwyg-quill',
+  props: {
+    toolbarOptions: {
+      type: [Array],
+      default: function (){
+        return [
+          { 'header': [2, 3, 4, 5, false] },
+          'bold',
+          'link',
+          'blockquote',
+          { 'list': 'ordered'},
+          { 'list': 'bullet' },
+          'image',
+          'code-block'
+        ]
+      }
+    }
+  },
   data () {
     return {
       quill: null
@@ -51,16 +70,8 @@ export default {
     let options = {
       debug: 'false',                                      // 'error', 'warn', 'log', or 'info'
       modules: {
-        // toolbar: '#editor-toolbar',
-        toolbar: [
-          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-          ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-          ['blockquote', 'code-block'],                     // 引用和code
-          [{ 'list': 'ordered'}, { 'list': 'bullet' }],     // 有序列表、无序列表
-          ['link', 'image'],                                // 连接、图片上传
-          ['clean']                                         // 清除格式
-        ]
-        // handlers: {}
+        // syntax: true,
+        toolbar: this.toolbarOptions
       },
       placeholder: 'Compose an epic...',
       readOnly: false,
